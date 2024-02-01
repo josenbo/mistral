3ddc265a26d2d0078d1dff420e551837b34bff5a
Stein,J.,SNL IT PeP, 1230, external <Jochen.Stein@deutschepost.de>
2024-02-02 00:56:26 +0100

    Create IDeploymentTransformationReadOnlyFile.cs

---
+++ src/vigobase/Transform/IDeploymentTransformationReadOnlyFile.cs
@@ -0,0 +1,15 @@
+﻿namespace vigobase;
+
+public interface IDeploymentTransformationReadOnlyFile : IDeploymentTransformationReadOnly
+{
+    FileInfo SourceFile { get; }
+    string RelativePathSourceFile { get; }
+    string? DifferentTargetFileName { get; }
+    FileInfo TargetFile { get; }
+    FileTypeEnum FileType { get; }
+    FileEncodingEnum SourceFileEncoding { get; }
+    FileEncodingEnum TargetFileEncoding { get; }
+    FilePermission FilePermission { get; }
+    LineEndingEnum LineEnding { get; }
+    bool FixTrailingNewline { get; }
+}
